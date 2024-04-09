23bd40f30fc911a412ec4c75113c61d5ab660ff9
APM_129 <llllllllizhen@gmail.com>
2024-04-09 15:13:51 +0800

    fix

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -7,6 +7,8 @@ interface ImportMetaEnv {
     readonly VITE_HF_QUERY_7D: string
     readonly VITE_GEO_API_URL: string
     readonly VITE_HF_KEY: string
+    readonly VITE_AMAP_IP_NAV_URL: string
+    readonly VITE_AMAP_KEY: string
   }
   
   interface ImportMeta {
